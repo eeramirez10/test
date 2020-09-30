758b3f5bad87b2e234fbbd7f9388b5b643db1e6c
eeramirez10 <eeramirez@tuvansa.com.mx>
2020-09-30 11:57:32 -0500

    Cuarto commit

--- src/routes/proscai.js
+++ src/routes/proscai.js
@@ -4,6 +4,6 @@ const router = require('express').Router();
 const proscaiController = require('../controllers/proscaiController');
 
 
-router.get('/server', proscaiController.list );
+router.get('/', proscaiController.list );
 
 module.exports = router;
