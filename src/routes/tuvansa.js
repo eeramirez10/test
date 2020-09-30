776b66da94ea052af43a91c09f171fe2159ee6a8
eeramirez10 <eeramirez@tuvansa.com.mx>
2020-09-30 12:32:00 -0500

    Se agregan cors a tuvansa controller

--- src/routes/tuvansa.js
+++ src/routes/tuvansa.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const cors = require('cors');
 const tuvansaController = require('../controllers/tuvansaController');
 
+router.use(cors());
 router.post('/data', tuvansaController.insert )
 
 module.exports = router;
