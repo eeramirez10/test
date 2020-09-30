f2f3a3d34007c0637566510c8798836e114782e7
eeramirez10 <eeramirez@tuvansa.com.mx>
2020-09-30 12:52:56 -0500

    noveno commit

--- src/routes/proscai.js
+++ src/routes/proscai.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 
 const proscaiController = require('../controllers/proscaiController');
+const tuvansaController = require('../controllers/tuvansaController');
 
 
 router.get('/server', proscaiController.list );
+
+router.post('/data', tuvansaController.insert )
 
 module.exports = router;
