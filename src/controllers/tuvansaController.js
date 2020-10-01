36a22e933302ddfde9846715ec01be80c675dad1
eeramirez10 <eeramirez@tuvansa.com.mx>
2020-10-01 10:57:28 -0500

    Probando de nuevo

--- src/controllers/tuvansaController.js
+++ src/controllers/tuvansaController.js
@@ -3,25 +3,15 @@ const mysql = require('mysql');
 const util = require('util');
 const moment = require('moment');
 
-const connection2 = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'tuvansa-server.dyndns.org',
     user: 'erick',
     password: 'Ag7348pp**',
-    database: 'tuvansa',
-    port: 3306
+    database: 'tuvansa'
 })
 
-connection2.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-
-    console.log('connected as id ' + connection2.threadId);
-});
-
 // Peticiones sincronas
-const query2 = util.promisify(connection2.query).bind(connection2);
+const query2 = util.promisify(connection.query).bind(connection);
 
 controller.insert = (req, res) => {
     let data = req.body;
@@ -38,33 +28,31 @@ controller.insert = (req, res) => {
         'ALMCANTREAL': data.value,
     };
 
-    console.log(inventarios);
+    (async () => {
 
-    /*     (async () => {
-    
-      
-            let busca = await query2(`SELECT * FROM inventarios WHERE ISEQ = ${inventarios.ISEQ}`);
-    
-            if (busca.length <= 0) {
-                let insert = await query2('INSERT INTO inventarios SET ?', inventarios)
-            } else {
-                let actualiza = await query2('UPDATE inventarios SET ALMCANTREAL = ?, IALTAREAL = ? WHERE ISEQ = ? ', [inventarios.ALMCANTREAL, inventarios.IALTAREAL, inventarios.ISEQ]);
-            }
-    
-    
-        })().catch(err => console.log(err)) */
+  
+        let busca = await query2(`SELECT * FROM inventarios WHERE ISEQ = ${inventarios.ISEQ}`);
+
+        if (busca.length <= 0) {
+            let insert = await query2('INSERT INTO inventarios SET ?', inventarios)
+        } else {
+            let actualiza = await query2('UPDATE inventarios SET ALMCANTREAL = ?, IALTAREAL = ? WHERE ISEQ = ? ', [inventarios.ALMCANTREAL, inventarios.IALTAREAL, inventarios.ISEQ]);
+        }
+
+
+    })().catch(err => console.log(err))
 }
 
-controller.agregaInventarioRealDBTuvansaAdataTable = async (output) => {
+ controller.agregaInventarioRealDBTuvansaAdataTable = async (output)=>{
     const out = await output;
 
     for (const data of out.aaData) {
-        let inventarios = await query2(`SELECT  * FROM inventarios WHERE iseq = ${data[0]}`);
-        if (inventarios.length > 0) {
-            data[7] = inventarios[0].ALMCANTREAL
-        }
+      let inventarios = await query2(`SELECT  * FROM inventarios WHERE iseq = ${data[0]}`);
+      if (inventarios.length > 0) {
+        data[7] = inventarios[0].ALMCANTREAL
+      }
     }
-
+  
     return out;
 }
 
